a2411a08dc47fe9af62031343da84017a125b4fd
cedric e goddard <8ceg6@comcast.net>
2019-08-14 15:47:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,20 @@
-def create_an_empty_array
+def create_an_empty_array 
+  []
   
 end
 
-def create_an_array
-  
+def create_an_array 
+  ["earth", "wind","fire","water"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+  array<< "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am","really","learning"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
